d20ec1529685754131caf96339bdb9487d5d057b
Yusron Hanan <yusronzain@gmail.com>
2021-07-18 08:43:19 +0700

    set up gh pages

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import css from 'rollup-plugin-css-only';
-
+import ghPages from 'gh-pages';
 const production = !process.env.ROLLUP_WATCH;
 
 function serve() {
@@ -68,7 +68,9 @@ export default {
 
 		// If we're building for production (npm run build
 		// instead of npm run dev), minify
-		production && terser()
+		production && terser() && ghPages.publish('public', (error) => {
+			console.log('published to github', error);
+		})
 	],
 	watch: {
 		clearScreen: false
